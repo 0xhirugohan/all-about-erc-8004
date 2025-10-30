2d6a0c7186d0fdaad3412c49dfe9f9d2deaa306a
hirugohan <han@fbrns.co>
2025-10-30 21:51:10 +0700

    feat(failed): view table for aggregating owners
    I was trying to create a view table using this reference
    https://ponder.sh/docs/schema/views#usage. But apparently it's not
    working and keep returning this error
    
    BuildError: Schema validation failed:
    view 'hourly_bucket.hour' is a non-column selected field.
    
    Despite I am using the exact code with the example. So after trying to
    downgrade the version and do some other approach, apparently I couldn't
    fix it. So this commit is to demonstrate how to achieve that error code.
    
    In the next commit, I will reverse and not use view table.

--- apps/indexer/ponder.config.ts
+++ apps/indexer/ponder.config.ts
@@ -14,8 +14,8 @@ export default createConfig({
       chain: "sepolia",
       abi: IdentityRegistryAbi,
       address: "0x8004a6090Cd10A7288092483047B097295Fb8847",
-      startBlock: 9420230, // testnet
-      // startBlock: 9499890, // dev
+      // startBlock: 9420230, // testnet
+      startBlock: 9499890, // dev
     },
   },
 });
--- apps/indexer/ponder.schema.ts
+++ apps/indexer/ponder.schema.ts
@@ -1,4 +1,11 @@
-import { onchainTable, primaryKey } from "ponder";
+import {
+  count,
+  onchainTable,
+  onchainView,
+  primaryKey,
+  sql,
+  sum,
+} from "ponder";
 
 export const agentIdentity = onchainTable(
   "agent_identity", (t) => ({
@@ -18,4 +25,33 @@ export const agentMetadata = onchainTable(
     value: t.text().notNull(),
   }),
   (table) => ({ pk: primaryKey({ columns: [table.chainId, table.tokenId, table.key] }) })
+);
+
+export const agentOwnerLeaderboard = onchainView("agent_owner_leaderboard").as((qb) =>
+  qb
+    .select({
+      ownerAddress: agentIdentity.ownerAddress,
+      // totalAgent: count().as("total_agent"),
+    })
+    .from(agentIdentity)
+    .groupBy(agentIdentity.ownerAddress)
+);
+
+export const transferEvent = onchainTable("transfer_event", (t) => ({
+  id: t.text().primaryKey(),
+  amount: t.bigint().notNull(),
+  timestamp: t.integer().notNull(),
+  from: t.hex().notNull(),
+  to: t.hex().notNull(),
+}));
+ 
+export const hourlyBucket = onchainView("hourly_bucket").as((qb) =>
+  qb
+    .select({
+      hour: sql`FLOOR(${transferEvent.timestamp} / 3600) * 3600`.as("hour"),
+      totalVolume: sum(transferEvent.amount).as("total_volume"),
+      transferCount: count().as("transfer_count"),
+    })
+    .from(transferEvent)
+    .groupBy(sql`FLOOR(${transferEvent.timestamp} / 3600)`)
 );
